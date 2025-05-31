c538dad5dc1215888e71ac06801331b8fc015af2
rafael-gino2 <rafaelgino27@outlook.com>
2025-05-31 18:25:07 -0300

    feat: adicionando nova atividade de python

---
+++ Estudos_def_Python/9.py
@@ -0,0 +1,9 @@
+def soma_impares(lista):
+    soma = 0
+    for numero in lista:
+        if numero % 2 != 0:
+            soma += numero
+    return soma
+numeros = [1, 2, 3, 4, 5, 6, 7]
+resultado = soma_impares(numeros)
+print("Soma dos números ímpares:", resultado)
